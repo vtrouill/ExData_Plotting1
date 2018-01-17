6ffdd3009d2ddbce31988cbd96b4de158082d5b2
vtrouill <vincent.trouillet@worldonline.fr>
2018-01-17 18:37:22 +0100

    forget corr in plot1.R

--- plot1.R
+++ plot1.R
@@ -7,7 +7,7 @@ if (!file.exists('./household_power_consumption.txt')) {
 # get data and clean
 fileName<-"./household_power_consumption.txt"
 colName<-c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
-powerConsumption<- read.table(text = grep("^[1,2]/2/2007",readLines(fileName),value=TRUE), sep = ';', na.strings = '?',col.names = colName)
+powerConso<- read.table(text = grep("^[1,2]/2/2007",readLines(fileName),value=TRUE), sep = ';', na.strings = '?',col.names = colName)
 
 #Open device 
 # png 480 px x 480 px
@@ -15,7 +15,7 @@ png(filename = "plot1.png", width = 480, height = 480, units = 'px' )
 
 #histogram of Global active power in red
 
-with(powerConsumption,hist(Global_active_power, main = "Global Active Power", xlab = "Global Active power (kilowatts) ", ylab = "Frequency", col="red"))
+with(powerConso,hist(Global_active_power, main = "Global Active Power", xlab = "Global Active power (kilowatts) ", ylab = "Frequency", col="red"))
 
 #close device
 dev.off()
